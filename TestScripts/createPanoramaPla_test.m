85445311cabdbf32a9cbf7347789b536b9bb45ae
malichan <ke_ma@outlook.com>
2015-03-14 02:47:37 -0500

    add InteractiveViewer

--- TestScripts/createPanoramaPla_test.m
+++ TestScripts/createPanoramaPla_test.m
@@ -1,6 +1,6 @@
 %% A test script to evaluate functions to create panoramas using the homography method
 %% setup vlfeat
-run(['../lib/vlfeat-0.9.20/toolbox/vl_setup']);
+run([pwd '/lib/vlfeat-0.9.20/toolbox/vl_setup']);
 
 %% non-loop test & no exposure matching & alpha blending
 imgFiles = {'TestImages/Test1-1.png', 'TestImages/Test1-2.png'};
@@ -29,6 +29,15 @@ imshow(newImg);
 imgFiles = {'TestImages/BascomTest2-1.jpg', 'TestImages/BascomTest2-2.jpg',...
     'TestImages/BascomTest2-3.jpg', 'TestImages/BascomTest2-4.jpg',...
     'TestImages/BascomTest2-5.jpg'};
+imgs = loadImages(imgFiles);
+newImg = createPanoramaPla(imgs, false, 'Pyramid');
+figure;
+imshow(newImg);
+
+%% Bascom final
+imgFiles = {'TestImages/Bascom2-1.jpg', 'TestImages/Bascom2-2.jpg',...
+    'TestImages/Bascom2-3.jpg', 'TestImages/Bascom2-4.jpg',...
+    'TestImages/Bascom2-5.jpg'};
 imgs = loadImages(imgFiles);
 newImg = createPanoramaPla(imgs, false, 'Pyramid');
 figure;
